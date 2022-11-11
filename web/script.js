d71aa00374447d756823290b12cfe05b63446725
kapilpokhrel <kapilpokhrel100@gmail.com>
2022-11-11 14:11:50 +0545

    resposive flexbox for canvas

--- web/script.js
+++ web/script.js
@@ -1,20 +1,62 @@
+var width = null;
+var height = null;
+
+function canvasResize() {
+    var testCanvas = document.getElementById("testCanvas");
+    var output = document.querySelector('.output');
+
+    var flexdirection = window.getComputedStyle(output).flexDirection;
+    if (width == null) {
+        if (flexdirection == 'row') {
+            testCanvas.style.width = '50%';
+            testCanvas.style.height = '100%';
+        } else {
+            testCanvas.style.width = '100%';
+            testCanvas.style.height = '50%';
+        }
+    }
+    else {
+        if (flexdirection == 'row') {
+            if (width > height) {
+                let canvasWidth = ~~Math.min(output.clientWidth * 50 / 100, width);
+                testCanvas.style.width = `${canvasWidth}px`;
+                testCanvas.style.height = `${height / width * canvasWidth}px`;
+            }
+            else {
+                let canvasHeight = Math.min(output.clientHeight, height);
+                testCanvas.style.height = `${canvasHeight}px`;
+                testCanvas.style.width = `${width / height * canvasHeight}px`;
+            }
+        } else {
+            if (width > height) {
+                let canvasWidth = Math.min(output.clientWidth, width);
+                canvasWidth = Math.min(canvasWidth, (width/height)*(output.clientHeight*0.5));
+                testCanvas.style.width = `${canvasWidth}px`;
+                testCanvas.style.height = `${height / width * canvasWidth}px`;
+            }
+            else {
+                let canvasHeight = Math.min(output.clientHeight * 50 / 100, height);
+                canvasHeight = Math.min(canvasHeight, (height/width)*(output.clientWidth));
+                testCanvas.style.height = `${canvasHeight}px`;
+                testCanvas.style.width = `${width / height * canvasHeight}px`;
+            }
+        }
+    }
+}
+
+window.onresize = canvasResize;
+
 function draw_on_desmos(data) {
     var triangles = data.triangles;
-    var width = data.width;
-    var height = data.height;
-    
+    width = data.width;
+    height = data.height;
+
     //Canvas settings
     var testCanvas = document.getElementById("testCanvas");
-    if(testCanvas == null) {
-        testCanvas = document.createElement("canvas");
-        testCanvas.id = "testCanvas";
-        testCanvas.width = width;
-        testCanvas.height = height;
-        testCanvas.style.width = '600px';
-        testCanvas.style.height = '600px';
+    testCanvas.width = width;
+    testCanvas.height = height;
+    canvasResize();
 
-        document.body.appendChild(testCanvas);
-    }
     var ctx = testCanvas.getContext('2d');
     ctx.clearRect(0, 0, testCanvas.width, testCanvas.height);
 
@@ -22,21 +64,25 @@ function draw_on_desmos(data) {
     //Desmos settings
     calculator.setBlank();
     var coords = calculator.graphpaperBounds.mathCoordinates;
-    var aRatio = coords.width/coords.height;
+    var aRatio = coords.width / coords.height;
     var xboundry, yboundry;
 
-    if(width > height) {{
-        xboundry = 50 + width;
-        yboundry = xboundry/aRatio;
-    }} else {{
-        yboundry = 50 + height;
-        xboundry = aRatio*yboundry;
-    }}
+    if (width > height) {
+        {
+            xboundry = 50 + width;
+            yboundry = xboundry / aRatio;
+        }
+    } else {
+        {
+            yboundry = 50 + height;
+            xboundry = aRatio * yboundry;
+        }
+    }
     calculator.setMathBounds({
         left: -xboundry,
         right: xboundry,
         top: yboundry,
-        bottom: -yboundry 
+        bottom: -yboundry
     });
     calculator.updateSettings({
         xAxisNumbers: false,
@@ -45,7 +91,7 @@ function draw_on_desmos(data) {
     });
 
     //Drawing triangles
-    for(let i = 0; i < triangles.length; i++) {
+    for (let i = 0; i < triangles.length; i++) {
         let vertices = triangles[i].vertices;
         let color = triangles[i].color;
 
@@ -62,9 +108,9 @@ function draw_on_desmos(data) {
         ctx.stroke();
 
         let latex = `\\\operatorname{polygon}(
-            (${vertices[0].x - width/2}, ${-(vertices[0].y - height/2)}),
-            (${vertices[1].x - width/2}, ${-(vertices[1].y - height/2)}),
-            (${vertices[2].x - width/2}, ${-(vertices[2].y - height/2)}))`;
+            (${vertices[0].x - width / 2}, ${-(vertices[0].y - height / 2)}),
+            (${vertices[1].x - width / 2}, ${-(vertices[1].y - height / 2)}),
+            (${vertices[2].x - width / 2}, ${-(vertices[2].y - height / 2)}))`;
         window.setTimeout(() => {
             calculator.setExpression({
                 latex: latex,
@@ -72,24 +118,24 @@ function draw_on_desmos(data) {
                 fill: true,
                 color: color
             }),
-            1
+                1
         });
-        
+
     }
 
 }
 
 function Process() {
     var imagefile = document.getElementById("image-file");
-    if(imagefile.value == "")
+    if (imagefile.value == "")
         alert("Image not selected");
     else {
         var file = imagefile.files[0];
         var edge_points = document.getElementById("edge-points").value;
         var bg_points = document.getElementById("bg-points").value;
-        edge_points = (edge_points <= 0) ? 1: edge_points;
-        bg_points = (bg_points < 0) ? 0: bg_points;
-        
+        edge_points = (edge_points <= 0) ? 1 : edge_points;
+        bg_points = (bg_points < 0) ? 0 : bg_points;
+
         var data = {
             file: file,
             points: {
@@ -99,9 +145,9 @@ function Process() {
         };
 
         var generator = new Worker("generator.js");
-        
+
         generator.postMessage(data);
-        generator.onmessage = function(msg) {
+        generator.onmessage = function (msg) {
             draw_on_desmos(msg.data);
         };
     }
